8cb29ad03ec944a143f6b966ca9c2d9b9358a645
rakesh.varandani <rkvarandani70@gmail.com>
2023-07-11 23:04:02 +0530

    first commit

---
+++ src/main/java/com/example/questionms/QuestionMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/questionms/QuestionMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
